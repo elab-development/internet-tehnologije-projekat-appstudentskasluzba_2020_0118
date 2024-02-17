f145c66197014982177abb8ed22bee57df856d84
Marko Milev <milev051@gmail.com>
2024-02-17 05:36:43 +0100

    Ubacen Context Provider sa tokenima za korisnike i deo css koda

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,29 @@
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider.jsx"
+
+export default function DefaultLayout() {
+
+    const { user, token } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />
+    }
+
+    return (
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/Dashboard">Dashboard</Link>
+                <Link to="/Users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>Header</div>
+                    <div>User info</div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    )
+}
